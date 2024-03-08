ea9b86ff6d7b395bff7cacd939bbf22bb9d7be2f
Augusto Mendes <ammguto@gmail.com>
2024-03-08 19:03:03 -0300

    "Código entradas e adição"

--- file1.js
+++ file1.js
@@ -1,4 +1,11 @@
-hello
-world
-sky
-teste
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question(`Qual o primeiro numero ->`, num1 => {
+    readline.question(`Qual o segundo numero ->`, num2 => {
+        let res = Number(num1)+Number(num2);
+        console.log(`a soma entre ${num1} e ${num2} é = ${res}`)
+    });
+});
