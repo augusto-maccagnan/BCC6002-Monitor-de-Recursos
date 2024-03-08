16a8913ae2dd56c8e8da63db54fb76eb02b4ea16
Augusto <ammguto@gmail.com>
2024-03-08 20:54:22 -0300

    Adicionada função soma

--- file1.js
+++ file1.js
@@ -3,9 +3,13 @@ const readline = require('readline').createInterface({
     output: process.stdout
 });
 
+function soma(a, b){
+    return Number(a)+Number(b);
+}
+
 readline.question(`Qual o primeiro numero ->`, num1 => {
     readline.question(`Qual o segundo numero ->`, num2 => {
-        let res = Number(num1)+Number(num2);
+        let res = soma(num1, num2)
         console.log(`a soma entre ${num1} e ${num2} é = ${res}`)
     });
 });
